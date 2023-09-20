5c605c63bef33623867a3dabc88a53ade89a28c4
sydney-pi <sydney.bang94@gmail.com>
2023-09-19 22:54:11 -0400

    added background

--- Player/Player.gd
+++ Player/Player.gd
@@ -20,11 +20,20 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
+	
+	if direction == -1:
+		get_node("AnimatedSprite2D").flip_h = true
+	elif direction == 1: 
+		get_node("AnimatedSprite2D").flip_h = false
+	
 	if direction:
 		velocity.x = direction * SPEED
-		anim.play("Run")
+		if velocity.y == 0:
+			anim.play("Run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		anim.play("Idle")
-
+		if velocity.y == 0:
+			anim.play("Idle")
+	if velocity.y > 0:
+		anim.play("Fall")
 	move_and_slide()
