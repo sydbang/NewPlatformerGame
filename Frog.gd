5ba08d9883832b408d618a86eb1522512a95ffe1
sydney-pi <sydney.bang94@gmail.com>
2023-09-23 17:12:06 -0400

    player death
    frog explodes and player dies

--- Frog.gd
+++ Frog.gd
@@ -5,19 +5,28 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var player
 var chase = false
 
+func _ready():
+	get_node("AnimatedSprite2D").play ("Idle")
+	
 func _physics_process(delta):
 	#Gravity for Frog
 	velocity.y += gravity * delta
 	if chase == true:
-		player = get_node("../../Player/Player")
-		var direction = (player.position - self.position).normalized()
-		if direction.x > 0:
-			get_node("AnimatedSprite2D").flip_h = true
-			velocity.x = direction.x * SPEED
+		if get_node("AnimatedSprite2D").animation != "Death":
+			get_node("AnimatedSprite2D").play ("Jump")
+			player = get_node("../../Player/Player")
+			var direction = (player.position - self.position).normalized()
+			if direction.x > 0:
+				get_node("AnimatedSprite2D").flip_h = true
+				velocity.x = direction.x * SPEED
 
-		else:
-			get_node("AnimatedSprite2D").flip_h = false
-			velocity.x = direction.x * SPEED
+			else:
+				get_node("AnimatedSprite2D").flip_h = false
+				velocity.x = direction.x * SPEED
+	else: 
+		if get_node("AnimatedSprite2D").animation != "Death":
+			get_node("AnimatedSprite2D").play ("Idle")
+		velocity.x = 0
 
 	move_and_slide()
 	
@@ -26,8 +35,24 @@ func _on_player_detection_body_entered(body):
 		chase = true
 		
 
-
-
 func _on_player_detection_body_exited(body):
 	if body.name == "Player":
 		chase = false
+
+
+func _on_player_death_body_entered(body):
+	if body.name == "Player":
+		death()
+		
+
+
+func _on_player_collision_body_entered(body):
+	if body.name == "Player":
+		body.health -= 3
+		death()
+		
+func death():
+	chase = false
+	get_node("AnimatedSprite2D").play ("Death")
+	await get_node("AnimatedSprite2D").animation_finished
+	self.queue_free()
--- Player/Player.gd
+++ Player/Player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-
+var health = 10
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
@@ -37,3 +37,8 @@ func _physics_process(delta):
 	if velocity.y > 0:
 		anim.play("Fall")
 	move_and_slide()
+	
+	if health <= 0:
+		queue_free()
+		get_tree().change_scene_to_file("res://main.tscn")
+		
